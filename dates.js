7a2c1eab16f7a1317b784573cf5833a53490855a
Robert Flaugher <rlfprog@gmail.com>
2015-08-06 09:35:11 -0700

    Added setters to dates

--- dates.js
+++ dates.js
@@ -19,3 +19,18 @@ console.log(halloween.getFullYear()); // 2015
 
 // Timestamp representing msec since the Epoch
 console.log(today.getTime());
+
+// Date setters
+var birthday = new Date();
+birthday.setMonth(5);  // Months indexed from 0
+birthday.setDate(8);
+console.log('Birthday is ' + birthday);
+
+var birthdate = new Date();
+// Doesn't account for timezone!
+birthdate.setMonth(5);  // Months indexed from 0
+birthdate.setDate(8);
+birthdate.setFullYear(1957);
+birthdate.setHours(06);
+birthdate.setMinutes(30);
+console.log('Born on ' + birthdate);
